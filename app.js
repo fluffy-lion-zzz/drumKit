dfdf5874295e377c78f2b45a44474238aa260afe
Code Nation <codenation@Codes-MBP.lan>
2019-06-06 17:45:48 +0100

    key finished, need stop function

--- app.js
+++ app.js
@@ -8,18 +8,13 @@ const ride = new Audio('sounds/ride.wav');
 const snare = new Audio('sounds/snare.wav');
 const tom = new Audio('sounds/tom.wav');
 const tink = new Audio('sounds/tink.wav');
+
 // Play drum by keyboard press
 
-// Play music by mouse click
-const clapA = document.getElementById("clapA");
-
-clapA.addEventListener('click', ()=>{
-    clap.play();
-})
-
+board.onkeyup.
 const clapAKey = document.body
 
-clapAKey.addEventListener('keypress', () => {
+clapAKey.addEventListener('keypress', (board) => {
     if (event.code =="KeyA") {
         clap.play()
     } else if (event.code =="KeyS") {
@@ -42,62 +37,56 @@ clapAKey.addEventListener('keypress', () => {
         return false
     }
 })
-
-
-
+// Play music by mouse click
+const clapA = document.getElementById("clapA");
+    clapA.addEventListener('click', ()=>{
+        clap.play();
+    })
 
 
 const hihatS = document.getElementById('hiHatS');
-
-hihatS.addEventListener('click', ()=>{
-    hihat.play();
-})
+    hihatS.addEventListener('click', ()=>{
+        hihat.play();
+    })
 
 
 const kickD = document.getElementById("kickD");
-
-kickD.addEventListener('click', ()=>{
-    kick.play();
-})
+    kickD.addEventListener('click', ()=>{
+        kick.play();
+    })
 
 
 const openHatF = document.getElementById("openHatF");
-
-openHatF.addEventListener('click', ()=>{
-    openHat.play();
-})
+    openHatF.addEventListener('click', ()=>{
+        openHat.play();
+    })
 
 
 const boomG = document.getElementById("boomG");
-
-boomG.addEventListener('click', ()=>{
-    boom.play();
-})
+    boomG.addEventListener('click', ()=>{
+        boom.play();
+    })
 
 
 const rideH = document.getElementById("rideH");
-
-rideH.addEventListener('click', ()=>{
-    ride.play();
-})
+    rideH.addEventListener('click', ()=>{
+        ride.play();
+    })
 
 
 const snareJ = document.getElementById("snareJ");
-
-snareJ.addEventListener('click', ()=>{
-    snare.play();
-})
+    snareJ.addEventListener('click', ()=>{
+        snare.play();
+    })
 
 
 const tomK = document.getElementById("tomK");
-
-tomK.addEventListener('click', ()=>{
-    tom.play();
-})
+    tomK.addEventListener('click', ()=>{
+        tom.play();
+    })
 
 
 const tinkL = document.getElementById("tinkL");
-
-tinkL.addEventListener('click', ()=>{
-    tink.play();
-})
+    tinkL.addEventListener('click', ()=>{
+        tink.play();
+    })
