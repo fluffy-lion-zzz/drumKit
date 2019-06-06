59e5ab55af452414aedef38f00e0c05a4cb3d6aa
Code Nation <codenation@Codes-MBP.lan>
2019-06-06 14:09:53 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+
+const clap = new Audio('sounds/snare.wav');
+const hihat = new Audio('sounds/hihat.wav');
+const kick = new Audio('sounds/kick.wav');
+const openHat = new Audio('sounds/openhat.wav');
+const boom = new Audio('sounds/boom.wav');
+const ride = new Audio('sounds/ride.wav');
+const snare = new Audio('sounds/snare.wav');
+const tom = new Audio('sounds/tom.wav');
+const tink = new Audio('sounds/tink.wav');
+// Play drum by keyboard press
+
+// Play music by mouse click
+const clapA = document.getElementById("clapA");
+
+clapA.addEventListener('click', ()=>{
+    clap.play();
+})
+
+
+
+const hihatS = document.getElementById('hiHatS');
+
+hihatS.addEventListener('click', ()=>{
+    hihat.play();
+})
+
+
+const kickD = document.getElementById("kickD");
+
+kickD.addEventListener('click', ()=>{
+    kick.play();
+})
+
+
+const openHatF = document.getElementById("openHatF");
+
+openHatF.addEventListener('click', ()=>{
+    openHat.play();
+})
+
+
+const boomG = document.getElementById("boomG");
+
+boomG.addEventListener('click', ()=>{
+    boom.play();
+})
+
+
+const rideH = document.getElementById("rideH");
+
+rideH.addEventListener('click', ()=>{
+    ride.play();
+})
+
+
+const snareJ = document.getElementById("snareJ");
+
+snareJ.addEventListener('click', ()=>{
+    snare.play();
+})
+
+
+const tomK = document.getElementById("tomK");
+
+tomK.addEventListener('click', ()=>{
+    tom.play();
+})
+
+
+const tinkL = document.getElementById("tinkL");
+
+tinkL.addEventListener('click', ()=>{
+    tink.play();
+})
